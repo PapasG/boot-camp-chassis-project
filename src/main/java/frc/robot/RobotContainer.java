d146decf6883490472badff0b0dddeeb2ebd0fe4
Gavriel Pappas <pappasgavriel@gmail.com>
2022-06-03 17:01:11 -0500

    fix drive train and robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,40 @@
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.DriveTrain;
 
 public class RobotContainer {
+
+  private final XboxController m_controller;
+  private final DriveTrain m_driveTrain;
+
+  private final JoystickButton m_AButton;
   
   public RobotContainer() {
-    // Configure the button bindings
+
+    m_driveTrain = new DriveTrain();
+    m_controller = new XboxController(0);
+
+    m_AButton = new JoystickButton(m_controller, XboxController.Button.kA.value);
+
     configureButtonBindings();
+    setDefaultCommands();
   }
 
   private void configureButtonBindings() {
 
+    
+
+  }
+
+  private void setDefaultCommands(){
+
+    m_driveTrain.setDefaultCommand(new RunCommand(() -> m_driveTrain.tankDrive(m_controller.getLeftY(),
+                                                                              m_controller.getRightY(),
+                                                                              false), m_driveTrain));
 
   }
 
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.MotorSafety;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase{
@@ -13,7 +16,26 @@ public class DriveTrain extends SubsystemBase{
 
     public DriveTrain(){
 
-        
+        Spark frontLeftDriveMotor = new Spark(3);
+        Spark frontRightDriveMotor = new Spark(0);
+        Spark backLeftDriveMotor = new Spark(2);
+        Spark backRightDriveMotor = new Spark(1);
+
+        m_leftMotorControllerGroup = new MotorControllerGroup(frontLeftDriveMotor, backLeftDriveMotor);
+        m_rightMotorControllerGroup = new MotorControllerGroup(frontRightDriveMotor, backRightDriveMotor);
+
+        m_differentialDrive = new DifferentialDrive(m_leftMotorControllerGroup, m_rightMotorControllerGroup);
+
+        m_leftMotorControllerGroup.setInverted(true);
+        m_rightMotorControllerGroup.setInverted(false);
+
+        m_differentialDrive.setSafetyEnabled(false);
+
+    }
+
+    public void tankDrive(double leftSpeed, double rightSpeed, boolean squareInputs){
+
+        m_differentialDrive.tankDrive(leftSpeed, rightSpeed, squareInputs);
 
     }
 
