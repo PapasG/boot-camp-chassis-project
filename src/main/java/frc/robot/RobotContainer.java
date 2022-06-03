e852344d1892cb7571d07d57cdeb749e2453e6c3
Gavriel Pappas <pappasgavriel@gmail.com>
2022-06-03 15:34:12 -0500

    saiuefghsdiugyfdsfigu]

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,26 @@
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+  
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  private void configureButtonBindings() {
+
+
+  }
+
+  public Command getAutonomousCommand() {
+    
+    return null;
+  
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase{
+
+    private final DifferentialDrive m_differentialDrive;
+
+    private final MotorControllerGroup m_leftMotorControllerGroup;
+    private final MotorControllerGroup m_rightMotorControllerGroup;
+
+    public DriveTrain(){
+
+        
+
+    }
+
+    
+}
